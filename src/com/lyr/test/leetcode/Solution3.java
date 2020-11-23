8bb5e1c60545f3c48de6f04be6fd592c1a13bef0
luyinren <18896994109@163.com>
2020-11-23 13:05:13 +0800

    无重复字符的最长子串

---
+++ src/com/lyr/test/leetcode/Solution3.java
@@ -0,0 +1,85 @@
+package com.lyr.test.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Author lyr
+ * @Date 2020-11-23 12:17
+ * @Version 1.0
+ * @Description 3. 无重复字符的最长子串
+ */
+public class Solution3 {
+    /**
+     * 滑动窗口
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        // 右指针，初始值为-1，相当于我们在字符串的左边界的左侧，还没开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                rk++;
+            }
+            // 第i到rk个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * HashMap优化
+     * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-jie-suan-fa-3-wu-zhong-fu-zi-fu-de-zui-chang-z/
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha) + 1, start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(alpha, end);
+        }
+        return ans;
+    }
+
+    /**
+     * 利用数组（桶）代替HashMap
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring3(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+        int res = 0;
+        // 窗口开始的位置
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+        return res;
+    }
+}
