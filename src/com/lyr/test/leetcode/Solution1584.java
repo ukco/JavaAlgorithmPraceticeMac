0a2ff2b8076b92e74ed4a756d792b0e855886011
luyinren <18896994109@163.com>
2021-01-19 11:41:13 +0800

    584. 连接所有点的最小费用

---
+++ src/com/lyr/test/leetcode/Solution1584.java
@@ -0,0 +1,90 @@
+package com.lyr.test.leetcode;
+
+import java.util.*;
+
+/**
+ * @Author lyr
+ * @Date 1/19/21 10:38 AM
+ * @Version 1.0
+ * @Description 1584. 连接所有点的最小费用
+ */
+public class Solution1584 {
+    /**
+     * Kruskal 算法
+     * @param points
+     * @return
+     */
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        DisjoinSetUnion dsu = new DisjoinSetUnion(n);
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                edges.add(new Edge(dist(points, i, j), i, j));
+            }
+        }
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge edge1, Edge edge2) {
+                return edge1.len - edge2.len;
+            }
+        });
+        int ret = 0, num = 1;
+        for (Edge edge : edges) {
+            int len = edge.len, x = edge.x, y = edge.y;
+            if (dsu.unionSet(x, y)) {
+                ret += len;
+                num++;
+                if (num == n) { break; }
+            }
+        }
+        return ret;
+    }
+
+    public int dist(int[][] points, int x, int y) {
+        return Math.abs(points[x][0] - points[y][0]) + Math.abs(points[x][1] - points[y][1]);
+    }
+}
+
+class DisjoinSetUnion {
+    int[] f;
+    int[] rank;
+    int n;
+
+    public DisjoinSetUnion(int n) {
+        this.n = n;
+        this.rank = new int[n];
+        Arrays.fill(this.rank, 1);
+        this.f = new int[n];
+        for (int i = 0; i < n; i++) {
+            this.f[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return f[x] == x ? x : (f[x] = find(f[x]));
+    }
+
+    public boolean unionSet(int x, int y) {
+        int fx = find(x), fy = find(y);
+        if (fx == fy) { return false; }
+        if (rank[fx] < rank[fy]) {
+            int temp = fx;
+            fx = fy;
+            fy = temp;
+        }
+        rank[fx] += rank[fy];
+        f[fy] = fx;
+        return true;
+    }
+}
+
+class Edge {
+    int len, x, y;
+
+    public Edge(int len, int x, int y) {
+        this.len = len;
+        this.x = x;
+        this.y = y;
+    }
+}
