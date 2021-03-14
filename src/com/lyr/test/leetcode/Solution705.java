772802e64cd1036aaa416e9f84cca153337675dd
luyinren <18896994109@163.com>
2021-03-14 13:54:47 +0800

    705. 设计哈希集合

---
+++ src/com/lyr/test/leetcode/Solution705.java
@@ -0,0 +1,66 @@
+package com.lyr.test.leetcode;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @Author lyr
+ * @Date 3/14/21 1:45 PM
+ * @Version 1.0
+ * @Description 705. 设计哈希集合
+ */
+public class Solution705 {
+
+}
+
+class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (key == element) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int hash(int key) {
+        return key % BASE;
+    }
+}
