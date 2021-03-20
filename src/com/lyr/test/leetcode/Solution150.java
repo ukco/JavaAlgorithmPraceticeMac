2ef86407fa1abfe490f1ef2a79e5bad24810c509
luyinren <18896994109@163.com>
2021-03-20 11:11:45 +0800

    150. 逆波兰表达式求值

---
+++ src/com/lyr/test/leetcode/Solution150.java
@@ -0,0 +1,87 @@
+package com.lyr.test.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author lyr
+ * @Date 3/20/21 10:37 AM
+ * @Version 1.0
+ * @Description 150. 逆波兰表达式求值
+ */
+public class Solution150 {
+    /**
+     * 栈
+     * @param tokens
+     * @return
+     */
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+        int n = tokens.length;
+        for (int i = 0; i < n; i++) {
+            String token = tokens[i];
+            if (isNumber(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                    default:
+                }
+            }
+        }
+        return stack.pop();
+    }
+    private boolean isNumber(String token) {
+        return !("+".equals(token) || "-".equals(token)
+                || "/".equals(token) || "*".equals(token));
+    }
+
+    /**
+     * 用数组模拟栈
+     * @param tokens
+     * @return
+     */
+    public int evalRPN2(String[] tokens) {
+        int n = tokens.length;
+        int[] stack = new int[(n + 1) / 2];
+        int index = -1;
+        for (int i = 0; i < n; i++) {
+            String token = tokens[i];
+            switch (token) {
+                case "+":
+                    index--;
+                    stack[index] += stack[index + 1];
+                    break;
+                case "-":
+                    index--;
+                    stack[index] -= stack[index + 1];
+                    break;
+                case "*":
+                    index--;
+                    stack[index] *= stack[index + 1];
+                    break;
+                case "/":
+                    index--;
+                    stack[index] /= stack[index + 1];
+                    break;
+                default:
+                    index++;
+                    stack[index] = Integer.parseInt(token);
+            }
+        }
+        return stack[index];
+    }
+}
